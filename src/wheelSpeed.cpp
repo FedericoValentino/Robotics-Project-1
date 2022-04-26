c2909e34a0752d175df844ca63815ed6d9e51025
sinnanzi <francesco.scroccarello@mail.polimi.it>
2022-04-26 16:16:19 +0200

    first version of the velocity publisher

--- src/wheelSpeed.cpp
+++ src/wheelSpeed.cpp
@@ -1,18 +1,42 @@
 #include "ros/ros.h"
 #include "geometry_msgs/TwistStamped.h"
+#include "project1/WheelSpeed.h"
+
+#define R 0.07
+#define L 0.2
+#define W 0.169
+#define CONV_FACTOR 3.14/30
 
 ros::Publisher pub;
 
-void printHere(const geometry_msgs::TwistStamped msg){
+void publishSpeed(const geometry_msgs::TwistStamped msg){
   /*ROS_INFO("data subscribed (%f %f %f), (%f %f %f)",
   msg.twist.linear.x,msg.twist.linear.y,msg.twist.linear.z,
   msg.twist.angular.x,msg.twist.angular.y,msg.twist.angular.z);*/
+  project1::WheelSpeed message;
+  message.Header=msg.header;
+  double vx=msg.twist.linear.x;
+  double vy=msg.twist.linear.y;
+  double omega=msg.twist.angular.z;
+
+  double vl1=CONV_FACTOR*(vx-vy-(L+W)*omega)/R;
+  double vr1=CONV_FACTOR*(vx+vy+(L+W)*omega)/R;
+  double vr2=CONV_FACTOR*(vx-vy+(L+W)*omega)/R;
+  double vl2=CONV_FACTOR*(vx+vy-(L+W)*omega)/R;
+
+  message.rpm_fl=vl1;
+  message.rpm_fr=vr1;
+  message.rpm_rl=vl2;
+  message.rpm_rr=vr2;
+
+  pub.publish(message);
 
 }
 
 int main(int argc, char**argv){
   ros :: init(argc,argv,"vector_print");
   ros :: NodeHandle node;
-  ros :: Subscriber sub=node.subscribe("/cmd_vel",1000,printHere);
+  pub=node.advertise<project1::WheelSpeed>("wheels_rpm",1000);
+  ros :: Subscriber sub=node.subscribe("/cmd_vel",1000,publishSpeed);
   ros :: spin();
 }
